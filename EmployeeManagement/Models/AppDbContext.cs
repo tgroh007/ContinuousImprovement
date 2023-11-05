d4ab23877e5b4e279edf8c4d7937d98f8f019794
LamboSlum <listallmobiles@gmail.com>
2023-11-05 12:27:45 -0800

    Add project files.

---
+++ EmployeeManagement/Models/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee
+                {
+                    Id = 1,
+                    Name = "Mark",
+                    Department = Dept.IT,
+                    Email = "lamboslum@gmail.com"
+                }
+            );
+        }
+    }
+}
